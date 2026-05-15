8b33b6f984e2c0baeee28362216375f40d7578e4
JG <jg.lofiego@gmail.com>
2026-05-15 19:32:05 -0300

    Refactor: Change name of files to be moead_apls

--- makeall.sh
+++ makeall.sh
@@ -5,7 +5,7 @@ directories=(
 )
 
 output_files=(
-    "moead_pls"
+    "moead_apls"
 )
 
 destination="./scripts/exe"
@@ -44,7 +44,7 @@ cd .. || { echo "Error returning to './scripts'"; exit 1; }
 scripts_dir="./smaller_scripts"
 scripts=(
     "create_folders.sh"
-    "moead_pls.sh"
+    "moead_apls.sh"
 )
 
 if [[ ! -d "$scripts_dir" ]]; then
